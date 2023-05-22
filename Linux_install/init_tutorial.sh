eaa97a9811d478c18a7b81f473204b9f34063378
mapemonde <olivier.duval@ensta-bretagne.org>
2023-05-22 13:43:57 +0200

    Solve conda unresolved command, by splitting installation into 2 scripts.

--- Linux_install/init_tutorial.sh
+++ Linux_install/init_tutorial.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+cd ..
 conda init
 . ~/.bashrc
-conda env create --name SOM_env -f lin_tutosub.yml
+conda env create --name SOM_env -f Linux_install/lin_tutosub.yml
 . ~/.bashrc
 source "$(conda info --base)"/etc/profile.d/conda.sh
 conda activate SOM_env
-cd ..
 jupyter notebook
